c7e8d83d0216b6ed90495881c1df319eb99f404e
amir-al-razin <amiralmohyminrazin@gmail.com>
2026-03-27 20:09:32 +0600

    Implement missing Customer upgrade API

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -1,4 +1,8 @@
 #include "Customer.h"
+#include "PremiumCustomer.h"
+
+#include <stdexcept>
+
 using namespace std;
 
 Customer::Customer(int id, const std::string& name) : id(id), name(name) {}
@@ -17,4 +21,24 @@ const vector<int>& Customer::getOrderHistory() const {
 
 void Customer::addOrderToHistory(int orderId) {
     orderHistory.push_back(orderId);
+}
+
+void Customer::upgradeToPremium(Customer*& customer, double loyaltyPercent) {
+    if (!customer) {
+        throw invalid_argument("Customer pointer is null.");
+    }
+    if (loyaltyPercent < 0.0 || loyaltyPercent >= 1.0) {
+        throw invalid_argument("Loyalty percent must be in [0.0, 1.0).");
+    }
+    if (dynamic_cast<PremiumCustomer*>(customer)) {
+        return;
+    }
+
+    auto* upgraded = new PremiumCustomer(customer->id, customer->name, loyaltyPercent);
+    for (int orderId : customer->orderHistory) {
+        upgraded->addOrderToHistory(orderId);
+    }
+
+    delete customer;
+    customer = upgraded;
 }
